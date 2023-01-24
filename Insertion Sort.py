7940652c1b194006fc187c2165e870dcd4ab0778
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 20:25:45 +0800

    array list and outputs are done

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -13,4 +13,15 @@ def insertion_sort(mynumber):
         while mynumber[j - 1] > mynumber[j] and j > 0:
             mynumber[j - 1], mynumber[j] = mynumber[j], mynumber[j - 1]
             j -= 1
+
             print(mynumber)
+
+
+mynumber = [62, 50, 95, 92, 24, 89, 15, 13, 8, 51]
+print("\nThe Original Array List for Insertion Sorting:\n", mynumber, "\n")
+
+print("The Process of Insertion Sorting:")
+insertion_sort(mynumber)
+
+print("\nThe Final sorted Number using Insertion Sorting:\n ", mynumber)
+
