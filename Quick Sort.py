9fd374f8e7ebb3efcdc794d97f0e1c37c407c5af
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 22:58:39 +0800

    if loop and greater and lower value was made

--- Quick Sort.py
+++ Quick Sort.py
@@ -7,3 +7,11 @@ print("**-------------------------------------------**")
 
 # The whole program is about Quick Sorting
 def quick_sort(seq):
+    length = len(seq)
+    if length <= 1:
+        return seq
+    else:
+        pivot = seq.pop()
+
+    greater_value = []
+    lower_value = []
