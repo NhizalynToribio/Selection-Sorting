ab11cd235b858cf6c14eef29e49b61b6eac78b27
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 20:14:34 +0800

    starting insertion sort code

---
+++ Insertion Sort.py
@@ -0,0 +1,7 @@
+print("************** PROGRAMMED BY: *****************")
+print("************* NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("*************** Insertion Sort  ****************")
+print("**--------------------------------------------**")
+
+# The whole program is about Insertion Sorting
