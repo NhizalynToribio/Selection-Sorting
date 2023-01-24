ce52325aa8125340451ca2013a84a2808962f4d0
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 22:56:56 +0800

    quick sort start def statement

---
+++ Quick Sort.py
@@ -0,0 +1,9 @@
+print("************** PROGRAMMED BY: *****************")
+print("************* NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("*************** Quick Sort  *******************")
+print("**-------------------------------------------**")
+
+
+# The whole program is about Quick Sorting
+def quick_sort(seq):
