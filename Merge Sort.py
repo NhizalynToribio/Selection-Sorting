b884919ae6e1ac58ba694ac9fe2c69b08a6532dd
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 21:15:22 +0800

    merge sort starter

---
+++ Merge Sort.py
@@ -0,0 +1,9 @@
+print("************** PROGRAMMED BY: *****************")
+print("************* NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("*************** Merge Sort  *******************")
+print("**-------------------------------------------**")
+
+# The whole program is about Insertion Sorting
+
+def merge_sort(array):
