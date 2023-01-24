29fd330d898888bcb4c90e6685c7fc2b968010b4
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 21:18:14 +0800

    if len)array and merge sort left and right is done

--- Merge Sort.py
+++ Merge Sort.py
@@ -4,6 +4,13 @@ print("*************** BSCOE 2 - 2 *******************")
 print("*************** Merge Sort  *******************")
 print("**-------------------------------------------**")
 
+
 # The whole program is about Insertion Sorting
 
 def merge_sort(array):
+    if len(array) > 1:
+        left_array = array[:len(array) // 2]
+        right_array = array[len(array) // 2:]
+
+        merge_sort(left_array)
+        merge_sort(right_array)
