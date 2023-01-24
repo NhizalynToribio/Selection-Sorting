e63cbc209299c633ef57f2d481de6daff5e72950
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 23:35:09 +0800

    modified done

--- Quick Sort.py
+++ Quick Sort.py
@@ -6,41 +6,41 @@ print("**-------------------------------------------**")
 
 
 # The whole program is about Quick Sorting
-def quick_sort(array, left, right):
+def quick_sort(mynumber, left, right):
     if left < right:
-        partition_position = partition(array, left, right)
-        quick_sort(array, left, partition_position - 1)
-        quick_sort(array, partition_position + 1, right)
+        partition_position = partition(mynumber, left, right)
+        quick_sort(mynumber, left, partition_position - 1)
+        quick_sort(mynumber, partition_position + 1, right)
 
 
-def partition(array, left, right):
+def partition(mynumber, left, right):
 
     i = left
     j = right - 1
-    pivot = array[right]
+    pivot = mynumber[right]
 
     while i < j:
 
-        while i < right and array[i] < pivot:
+        while i < right and mynumber[i] < pivot:
             i += 1
-        while j > left and array[j] >= pivot:
+        while j > left and mynumber[j] >= pivot:
             j -= 1
         if i < j:
-            array[i], array[j] = array[j], array[i]
+            mynumber[i], mynumber[j] = mynumber[j], mynumber[i]
 
-    if array[i] > pivot:
-        array[i], array[right] = array[right], array[i]
+    if mynumber[i] > pivot:
+        mynumber[i], mynumber[right] = mynumber[right], mynumber[i]
 
-    print(array)
+    print(mynumber)
 
     return i
 
 
-array = [62, 50, 95, 92, 24, 89, 15, 13, 8, 51]
-print("\nThe Original Array List for Quick Sorting:\n", array, "\n")
+mynumber = [62, 50, 95, 92, 24, 89, 15, 13, 8, 51]
+print("\nThe Original Array List for Quick Sorting:\n", mynumber, "\n")
 
 print("The Process of Quick Sorting:")
-quick_sort(array, 0, len(array) - 1)
+quick_sort(mynumber, 0, len(mynumber) - 1)
 
-print("\nThe Final sorted Number using Quick Sorting:\n ", array)
+print("\nThe Final sorted Number using Quick Sorting:\n ", mynumber)
 
