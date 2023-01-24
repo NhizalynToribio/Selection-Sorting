c8104901bc7ca6fd68e7aba63581970177eb1f18
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-24 20:23:39 +0800

    insertion sort def is made with for looping for the process of sorting

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -4,4 +4,13 @@ print("*************** BSCOE 2 - 2 *******************")
 print("*************** Insertion Sort  ****************")
 print("**--------------------------------------------**")
 
+
 # The whole program is about Insertion Sorting
+
+def insertion_sort(mynumber):
+    for i in range(1, len(mynumber)):
+        j = i
+        while mynumber[j - 1] > mynumber[j] and j > 0:
+            mynumber[j - 1], mynumber[j] = mynumber[j], mynumber[j - 1]
+            j -= 1
+            print(mynumber)
